f37b95098f177de94986585dfbd0cea2a9d5c785
Akshat Mehta <akshatmehta@Akshats-Air.fios-router.home>
2021-07-26 09:38:44 -0400

    Added basic bot structure

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is Ready')
+
+client.run('ODY4ODc0ODgzODc3MTgzNTA5.YP2Ayw.NRmrr4JtsApI0UxoIbWiS6NfC08')
